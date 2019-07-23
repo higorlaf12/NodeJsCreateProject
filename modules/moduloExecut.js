3957f10f8553a6b32e30e255e0c5eb3a00c6bc69
HigorLucas <higor.freitas@neppoadm.com.br>
2019-07-23 11:06:58 -0300

    Correções rapidas inserindo somente um log dos modulos

--- modules/moduloExecut.js
+++ modules/moduloExecut.js
@@ -4,6 +4,8 @@ const moduloB = require ('./moduloB');
 console.log(moduloA.olaMundo);
 console.log(moduloA.informacoa);
 console.log(moduloA.aprenseteAlguamInformacao);
+console.log(moduloA);
 
 console.log(moduloB.bomDia);
 console.log(moduloB.boaNoite());
+console.log(moduloB);
