5861befb3cf573d9bf400b71a0e5a7b1cdb3ac50
HigorLucas <higor.freitas@neppoadm.com.br>
2019-07-23 19:41:54 -0300

    Resolvido o problema

--- modules/funcionarioMenorSalario.js
+++ modules/funcionarioMenorSalario.js
@@ -2,7 +2,48 @@
 const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json';
 const axios = require('axios');
 
-axios.get(url).then(response => {
-    const funcionarios = response.data
-    console.log(funcionarios)
-})
+// Maneira feita por mim
+// let guardarMulher =[];
+// let menor = 0;
+//
+// const buscaMenorSalario = (vetor) =>{
+//     for( let i = 1; i < vetor.length ; i++){
+//         if(vetor[i].salario < vetor[menor].salario){
+//             menor = i;
+//         }
+//
+//     }
+//     return vetor[menor];
+// }
+//
+// axios.get(url).then(response => {
+//
+//     const funcionarios = response.data;
+//     for (let index in funcionarios){
+//         if (funcionarios[index].genero === 'F'){
+//             if (funcionarios[index].pais === 'China'){
+//                 guardarMulher.push(funcionarios[index]);
+//             }
+//         }
+//     }
+//
+//     console.log(buscaMenorSalario(guardarMulher));
+// });
+
+// Maneira feita pelo instrutor
+
+const chinesas = f => f.pais === 'China';
+const mulher = f => f.genero === 'F';
+const menorSalario = (func,funcAtual) => {
+    return func.salario < funcAtual.salario ? func : funcAtual;
+};
+
+axios.get(url).then( response =>{
+    const funcionarios = response.data;
+    const func = funcionarios
+        .filter(chinesas)
+        .filter(mulher)
+        .reduce(menorSalario);
+
+    console.log(func)
+});
